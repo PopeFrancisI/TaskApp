d55de28c90176dc47726d5f5140898da59f35577
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-07-25 21:15:18 +0200

    added loadAll() method that gets all data from api and adds it as html elements

--- js/domElements.js
+++ js/domElements.js
@@ -7,4 +7,12 @@ class DomElements {
 
         this.loadAll();
     }
+
+    loadAll(){
+        this.apiService.getTasks(tasks => {
+            tasks.map( task => {
+                this.createTaskElement(task);
+            });
+        }).catch( error => {console.log(error)});
+    }
 }
